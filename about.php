93a6cad2c3bc620f8c8f4a3216178fcdc9279563
shivaraj5 <abshivaraj555@gmail.com>
2022-07-12 14:13:32 +0530

    All files have been added

---
+++ about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <!--jQuery library-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!--Latest compiled and minified JavaScript-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="ministylesheet.css">
+
+    <title>HOSPITAL MANAGEMENT SYSTEM</title>
+
+</head>
+
+<body>
+    <section class="text-gray-700 body-font">
+        <div class="container px-5 py-24 mx-auto flex flex-wrap">
+            <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+                <img alt="feature" class="object-cover object-center h-full w-full" src="https://www.pngjoy.com/pngl/922/10739741_doctors-health-doctor-png-hd-png-download.png">
+            </div>
+            <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+                <div class="flex flex-col mb-10 lg:items-start items-center">
+                    <div class="container" id="loginpage">
+                        <div class="row">
+                            <div>
+                                <div class="panel panel-info">
+                                    <div class="panel-heading">
+                                        <p style="font-size:72px;text-align: center;color:blue;">ABOUT US</p>
+                                    </div>
+                                    <div class="panel-body">
+                                        <ol>
+                                            <p>
+                                                <li style="font-size:16px;text-align: center;color:black;">Hey patients! You don't need to worry about how to choose the best doctors accordance with your disease.
+                                                    We will provide you you the best doctors in our website.</li> <br>
+                                                <li style="font-size:16px;text-align: center;color:black;">Our website is patient friendly.We provide patients the doctor's qualification details so that they can choose the genuinity of doctors.</li><br>
+                                                <li style="font-size:16px;text-align: center;color:black;">We also provide the years of experience of doctors so that patients can decide which doctor will be suitable for their operation or surgery or to cure disease.</li><br>
+                                                <li style="font-size:16px;text-align: center;color:black;">We also provide the fee details of all the doctors so that patients can choose the best doctor according to their own budget.</li><br>
+                                            </p>
+                                        </ol>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container">
+    <h1>Group Details</h1>
+        <ul>
+            <li><h4>SHIVA RAJ (CSE at BIET JHANSI)</h4></li>
+            <li><h4>AYUSH KUMAR SHUKLA (CSE at BIET JHANSI)</h4></li>
+            <li><h4>SHUBHAM KUMAR MAURYA (CSE at BIET JHANSI)</h4></li>     
+        </ul>
+    </div>
+    
+</body>
